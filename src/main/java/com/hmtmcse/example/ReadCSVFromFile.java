234c4ef8c91c359d2f947bbda548e06c7fd5e244
Touhid Mia <hmtm.cse@gmail.com>
2020-06-19 14:22:25 +0600

    Open CSV

---
+++ src/main/java/com/hmtmcse/example/ReadCSVFromFile.java
@@ -0,0 +1,25 @@
+package com.hmtmcse.example;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSVFromFile {
+
+
+    public static void main(String[] args) {
+        CSVReader csvReader = null;
+        try {
+            csvReader = new CSVReader(new FileReader("country.csv"));
+            String[] line;
+            while ((line = csvReader.readNext()) != null) {
+                System.out.println("Country id= " + line[0] + ", code= " + line[1] + " , name=" + line[2]);
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
